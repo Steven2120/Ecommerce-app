1636a4bb0aad0958acfda473edaf3a45be9ddba6
Steven Moyano <stevenmoyano1@gmail.com>
2021-12-09 11:00:04 -0600

    continued work on navbar. Added styling

--- ecom-client/src/components/Navigation/Navbar.js
+++ ecom-client/src/components/Navigation/Navbar.js
@@ -3,11 +3,29 @@ import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div className="navbar">
+    <nav className="navbar">
       <div className="navbar__logo">
         <h2>Ecom Shopping Cart</h2>
       </div>
-    </div>
+      <ul className="navbar__links">
+        <li>
+          <i className="fas fa-shopping-cart"></i>
+          <span>
+            Cart
+            <span className="cartlogo__badge">(0)</span>
+          </span>
+        </li>
+        <li>
+          <Link to="/">Return to Shop</Link>
+        </li>
+      </ul>
+
+      <div className="hamburger__menu">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </nav>
   );
 };
 
