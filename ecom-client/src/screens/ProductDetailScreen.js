ba6ac54a1afa019567b88c3c9a789a3b83660f85
Steven Moyano <stevenmoyano1@gmail.com>
2021-12-10 14:44:52 -0600

    worked on Product detail screen. hard coded values and styled. Will add redux later to make all values dynamic

--- ecom-client/src/screens/ProductDetailScreen.js
+++ ecom-client/src/screens/ProductDetailScreen.js
@@ -1,7 +1,51 @@
 import React from "react";
+import "./ProductDetailScreen.css";
 
 const ProductDetailScreen = () => {
-  return <div>product detail screen</div>;
+  return (
+    <div className="detail__screen">
+      <div className="details__left">
+        <div className="image">
+          <img
+            src="https://ecom-app-product-images.s3.amazonaws.com/shiba_pillow.jpg"
+            alt="shibainu pillow"
+          />
+        </div>
+
+        <div className="info__left">
+          <p className="name__left">Product 1</p>
+          <p>Price: $499.99</p>
+          <p>
+            Description: Lorem, ipsum dolor sit amet consectetur adipisicing
+            elit. Laborum, porro?
+          </p>
+        </div>
+      </div>
+
+      <div className="details__right">
+        <div className="info__right">
+          <p>
+            Price: <span>$499.99</span>
+          </p>
+          <p>
+            Status: <span>In Stock</span>
+          </p>
+          <p>
+            Qty
+            <select name="" id="">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+            </select>
+          </p>
+          <p>
+            <button type="button">Add To Cart</button>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetailScreen;
