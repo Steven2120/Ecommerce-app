3605b913feb0c69288f2aa02ce7e0790f7381ae0
Steven Moyano <stevenmoyano1@gmail.com>
2022-01-11 15:44:12 -0600

    continued work on forgot password by implementing a function that will assist user when they click on forgot password. added nodemailer and sendgrid and add proper environment variables for security

--- ecom-server/utils/emailSender.js
+++ ecom-server/utils/emailSender.js
@@ -1,1 +1,28 @@
 const nodemailer = require("nodemailer");
+
+const emailSender = (options) => {
+  const transporter = nodemailer.createTransport({
+    service: process.env.EMAIL_SERVICE,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      password: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_SENDER,
+    to: options.to,
+    subject: options.subject,
+    html: options.text,
+  };
+
+  transporter.sendMail(mailOptions, function (err, info) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(info);
+    }
+  });
+};
+
+module.exports = emailSender;
